a57574be85fb529635958b4a8a3b2f92f38c5581
Huangjie <huangjie1663@phytium.com.cn>
2025-04-24 17:26:37 +0800

    huile-c3: add pwm, remove display and press_to_talk
    Signed-off-by: Huangjie <huangjie1663@phytium.com.cn>

--- main/boards/huile-c3/huile_c3_board.cc
+++ main/boards/huile-c3/huile_c3_board.cc
@@ -16,11 +16,79 @@
 #include <driver/i2c_master.h>
 #include <esp_lcd_panel_ops.h>
 #include <esp_lcd_panel_vendor.h>
+#include <driver/ledc.h>
+#include <esp_check.h>
 
 #define TAG "huile_c3"
 
 LV_FONT_DECLARE(font_puhui_14_1);
 LV_FONT_DECLARE(font_awesome_14_1);
+
+class PWM {
+private:
+    ledc_timer_t timer_num_;
+    uint32_t freq_hz_;
+    ledc_timer_bit_t duty_resolution_;
+    ledc_mode_t speed_mode_;
+    ledc_channel_t channel_;
+    uint32_t duty_;
+    uint8_t duty_to_percent_;
+    gpio_num_t gpio_;
+
+public:
+
+    esp_err_t setupPWM(ledc_timer_t timer_num, uint32_t freq_hz,
+        ledc_timer_bit_t duty_resolution, ledc_mode_t speed_mode,
+        ledc_channel_t channel, uint32_t duty, gpio_num_t gpio) {
+
+        esp_err_t ret = ESP_OK;
+
+        this->timer_num_ = timer_num;
+        this->freq_hz_ = freq_hz;
+        this->duty_resolution_ = duty_resolution;
+        this->speed_mode_ = speed_mode;
+        this->channel_ = channel;
+        this->duty_ = duty;
+        this->gpio_ = gpio;
+
+        ledc_timer_config_t timer_config = {
+            .speed_mode = this->speed_mode_,
+            .duty_resolution = this->duty_resolution_,
+            .timer_num = this->timer_num_,
+            .freq_hz = this->freq_hz_,
+            .clk_cfg = LEDC_AUTO_CLK,
+        };
+
+        ledc_channel_config_t channel_config = {
+            .gpio_num = this->gpio_,
+            .speed_mode = this->speed_mode_,
+            .channel = this->channel_,
+            .intr_type = LEDC_INTR_DISABLE,
+            .timer_sel = this->timer_num_,
+            .duty = this->duty_,
+        };
+
+        ret = ledc_timer_config(&timer_config);
+        ESP_RETURN_ON_ERROR(ret, TAG, "ledc timer config failed");
+        ret = ledc_channel_config(&channel_config);
+        ESP_RETURN_ON_ERROR(ret, TAG, "ledc channel config failed");
+
+        return ret;
+    }
+
+    esp_err_t setDuty(uint8_t duty) {
+        esp_err_t ret = ESP_OK;
+
+        this->duty_to_percent_ = duty;
+        this->duty_ = (1 << this->duty_resolution_) * duty / 100;
+        ret = ledc_set_duty(this->speed_mode_, this->channel_, this->duty_);
+        ESP_RETURN_ON_ERROR(ret, TAG, "set duty failed");
+        ret = ledc_update_duty(this->speed_mode_, this->channel_);
+        ESP_RETURN_ON_ERROR(ret, TAG, "update duty failed");
+
+        return ret;
+    };
+};
 
 class HuileC3Board : public WifiBoard {
 private:
@@ -28,8 +96,11 @@ private:
     esp_lcd_panel_io_handle_t panel_io_ = nullptr;
     esp_lcd_panel_handle_t panel_ = nullptr;
     Display* display_ = nullptr;
-    Button boot_button_;
-    bool press_to_talk_enabled_ = false;
+    Button touch_button_;
+    Button switch_button_;
+    PWM motor_pwm_;
+    PWM mag_pwm_;
+    bool lift_up_ = false;
     PowerSaveTimer* power_save_timer_;
 
     void InitializePowerSaveTimer() {
@@ -71,94 +142,48 @@ private:
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
     }
 
-    void InitializeSsd1306Display() {
-        // SSD1306 config
-        esp_lcd_panel_io_i2c_config_t io_config = {
-            .dev_addr = 0x3C,
-            .on_color_trans_done = nullptr,
-            .user_ctx = nullptr,
-            .control_phase_bytes = 1,
-            .dc_bit_offset = 6,
-            .lcd_cmd_bits = 8,
-            .lcd_param_bits = 8,
-            .flags = {
-                .dc_low_on_data = 0,
-                .disable_control_phase = 0,
-            },
-            .scl_speed_hz = 400 * 1000,
-        };
-
-        ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c_v2(codec_i2c_bus_, &io_config, &panel_io_));
-
-        ESP_LOGI(TAG, "Install SSD1306 driver");
-        esp_lcd_panel_dev_config_t panel_config = {};
-        panel_config.reset_gpio_num = -1;
-        panel_config.bits_per_pixel = 1;
-
-        esp_lcd_panel_ssd1306_config_t ssd1306_config = {
-            .height = static_cast<uint8_t>(DISPLAY_HEIGHT),
-        };
-        panel_config.vendor_config = &ssd1306_config;
-
-        ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(panel_io_, &panel_config, &panel_));
-        ESP_LOGI(TAG, "SSD1306 driver installed");
-
-        // Reset the display
-        ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_));
-        if (esp_lcd_panel_init(panel_) != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to initialize display");
-            display_ = new NoDisplay();
-            return;
-        }
-
-        // Set the display to on
-        ESP_LOGI(TAG, "Turning display on");
-        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
-
-        display_ = new OledDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y,
-            {&font_puhui_14_1, &font_awesome_14_1});
-    }
-
     void InitializeButtons() {
-        boot_button_.OnClick([this]() {
+        touch_button_.OnClick([this]() {
             auto& app = Application::GetInstance();
             if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
                 ResetWifiConfiguration();
             }
-            if (!press_to_talk_enabled_) {
                 app.ToggleChatState();
-            }
         });
-        boot_button_.OnPressDown([this]() {
-            power_save_timer_->WakeUp();
-            if (press_to_talk_enabled_) {
-                Application::GetInstance().StartListening();
-            }
+
+        switch_button_.OnPressDown([this]() {
+            lift_up_ = true;
         });
-        boot_button_.OnPressUp([this]() {
-            if (press_to_talk_enabled_) {
-                Application::GetInstance().StopListening();
-            }
+
+        switch_button_.OnPressUp([this]() {
+            lift_up_ = false;
         });
     }
 
     // 物联网初始化，添加对 AI 可见设备
     void InitializeIot() {
-        Settings settings("vendor");
-        press_to_talk_enabled_ = settings.GetInt("press_to_talk", 0) != 0;
-
         auto& thing_manager = iot::ThingManager::GetInstance();
         thing_manager.AddThing(iot::CreateThing("Speaker"));
-        thing_manager.AddThing(iot::CreateThing("PressToTalk"));
+    }
+
+    void InitializePWM() {
+        motor_pwm_.setupPWM(LEDC_TIMER_0, 4000, LEDC_TIMER_13_BIT,
+                            LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0,
+                            0, MOTOR_PWM_GPIO);
+        mag_pwm_.setupPWM(LEDC_TIMER_1, 4000, LEDC_TIMER_13_BIT,
+                            LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1,
+                            0, MAG_PWM_GPIO);
     }
 
 public:
-    HuileC3Board() : boot_button_(TOUCH_BUTTON_GPIO) {  
+    HuileC3Board() : touch_button_(TOUCH_BUTTON_GPIO),
+                     switch_button_(SWITCH_GPIO, true) {
         // 把 ESP32C3 的 VDD SPI 引脚作为普通 GPIO 口使用
         esp_efuse_write_field_bit(ESP_EFUSE_VDD_SPI_AS_GPIO);
 
+        display_ = new NoDisplay();
+
         InitializeCodecI2c();
-        InitializeSsd1306Display();
         InitializeButtons();
         InitializePowerSaveTimer();
         InitializeIot();
@@ -179,18 +204,6 @@ public:
             AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
         return &audio_codec;
     }
-
-    void SetPressToTalkEnabled(bool enabled) {
-        press_to_talk_enabled_ = enabled;
-
-        Settings settings("vendor", true);
-        settings.SetInt("press_to_talk", enabled ? 1 : 0);
-        ESP_LOGI(TAG, "Press to talk enabled: %d", enabled);
-    }
-
-    bool IsPressToTalkEnabled() {
-        return press_to_talk_enabled_;
-    }
 };
 
 DECLARE_BOARD(HuileC3Board);
@@ -198,26 +211,4 @@ DECLARE_BOARD(HuileC3Board);
 
 namespace iot {
 
-class PressToTalk : public Thing {
-public:
-    PressToTalk() : Thing("PressToTalk", "控制对话模式，一种是长按对话，一种是单击后连续对话。") {
-        // 定义设备的属性
-        properties_.AddBooleanProperty("enabled", "true 表示长按说话模式，false 表示单击说话模式", []() -> bool {
-            auto board = static_cast<HuileC3Board*>(&Board::GetInstance());
-            return board->IsPressToTalkEnabled();
-        });
-
-        // 定义设备可以被远程执行的指令
-        methods_.AddMethod("SetEnabled", "启用或禁用长按说话模式，调用前需要经过用户确认", ParameterList({
-            Parameter("enabled", "true 表示长按说话模式，false 表示单击说话模式", kValueTypeBoolean, true)
-        }), [](const ParameterList& parameters) {
-            bool enabled = parameters["enabled"].boolean();
-            auto board = static_cast<HuileC3Board*>(&Board::GetInstance());
-            board->SetPressToTalkEnabled(enabled);
-        });
-    }
-};
-
 } // namespace iot
-
-DECLARE_THING(PressToTalk);
