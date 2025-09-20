d6054584eeee6661f0e3a53c143e148eab349128
Jack Huang <jackhuang021@gmail.com>
2025-09-21 02:04:44 +0800

    board: esp_ai: support to set lcd brightness by pmic axp2101
    Signed-off-by: Jack Huang <jackhuang021@gmail.com>

--- main/boards/esp_ai/esp_ai_board.cc
+++ main/boards/esp_ai/esp_ai_board.cc
@@ -6,7 +6,7 @@
 #include "config.h"
 #include "i2c_device.h"
 #include "esp32_camera.h"
-#include "axp2101.h"
+#include "pmic.h"
 
 #include <esp_log.h>
 #include <esp_lcd_panel_vendor.h>
@@ -17,65 +17,7 @@
 #include <esp_lvgl_port.h>
 #include <lvgl.h>
 
-
-#define TAG "esp_ai"
-
-class Pmic : public Axp2101 {
-public:
-    Pmic(i2c_master_bus_handle_t i2c_bus, uint8_t addr) : Axp2101(i2c_bus, addr) {
-        WriteReg(0x22, 0b110); // PWRON > OFFLEVEL as POWEROFF Source enable
-        WriteReg(0x27, 0x10);  // hold 4s to power off
-
-        // Disable All DCs but DC1
-        WriteReg(0x80, 0x01);
-        // Disable All LDOs
-        WriteReg(0x90, 0x00);
-        WriteReg(0x91, 0x00);
-
-        // Set DC1 to 3.3V
-        WriteReg(0x82, (3300 - 1500) / 100);
-
-        // Set ALDO1 to 3.3V AVDD
-        WriteReg(0x92, (3300 - 500) / 100);
-
-        // Set ALDO4 to 3.3V LCD_BL
-        WriteReg(0x95, (3300 - 500) / 100);
-
-        // Set BLDO2 to 3.3V PA_EN
-        WriteReg(0x97, (3300 - 500) / 100);
-
-        // Set DLDO1 to 3.3V LCD_CS
-         WriteReg(0x99, (3300 - 500) / 100);
-
-        // Enable ALDO1(MIC) & Set LCD_CS High & PA_EN
-        WriteReg(0x90, 0xA9);
-
-        WriteReg(0x64, 0x02); // CV charger voltage setting to 4.1V
-        
-        WriteReg(0x61, 0x02); // set Main battery precharge current to 50mA
-        WriteReg(0x62, 0x08); // set Main battery charger current to 400mA ( 0x08-200mA, 0x09-300mA, 0x0A-400mA )
-        WriteReg(0x63, 0x01); // set Main battery term charge current to 25mA
-    }
-
-    void set_lcd_cs(bool state) {
-        uint8_t value = ReadReg(0x90);
-        if (state)
-            value |= BIT(7);
-        else
-            value &= ~BIT(7);
-        WriteReg(0x90, value);
-    }
-
-    void set_lcd_backlight(bool state) {
-        uint8_t value = ReadReg(0x90);
-        if (state)
-            value |= BIT(3);
-        else
-            value &= ~BIT(3);
-        WriteReg(0x90, value);
-    }
-};
-
+static const char *TAG = "esp_ai";
 
 class CustomAudioCodec : public BoxAudioCodec {
 
@@ -135,7 +77,7 @@ private:
         ESP_LOGI(TAG, "Init AXP2101");
         pmic_ = new Pmic(i2c_bus_, 0x34);
         pmic_->set_lcd_cs(true);
-        pmic_->set_lcd_backlight(true);
+        pmic_->set_lcd_backlight(50);
         vTaskDelay(pdMS_TO_TICKS(300));
     }
 
@@ -287,7 +229,7 @@ public:
         InitializeButtons();
         // InitializeCamera();
 
-        // GetBacklight()->RestoreBrightness();
+        GetBacklight()->RestoreBrightness();
     }
 
     virtual AudioCodec* GetAudioCodec() override {
@@ -300,10 +242,10 @@ public:
         return display_;
     }
     
-    // virtual Backlight* GetBacklight() override {
-    //     static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
-    //     return &backlight;
-    // }
+    virtual Backlight* GetBacklight() override {
+        static PmicBacklight backlight(pmic_);
+        return &backlight;
+    }
 
     // virtual Camera* GetCamera() override {
     //     return camera_;
